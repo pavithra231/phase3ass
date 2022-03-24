7c0f4c4c44fa836c800834c1363642d79a391049
pavithra231 <98319638+pavithra231@users.noreply.github.com>
2022-03-24 10:55:43 +0530

    Add files via upload

---
+++ SpringSecurity/src/main/java/com/project/SpringSecurity/entities/User.java
@@ -0,0 +1,58 @@
+package com.project.SpringSecurity.entities;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="user")
+public class User {
+
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	private Integer id;
+	
+	private String name;
+	
+	private String password;
+	
+	private String role;
+
+	
+	public User() {
+		super();
+	}
+
+	public User(String name, String password,String role) {
+		super();
+		this.name = name;
+		this.password = password;
+		this.role = role;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getRole() {
+		return role;
+	}
+
+	public void setRole(String role) {
+		this.role = role;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+}
